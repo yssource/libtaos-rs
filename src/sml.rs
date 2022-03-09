2d3b98553aa64e62c3610351f9408dee7cd92e63
Huo Linhe <linhehuo@gmail.com>
2022-03-09 16:22:46 +0800

    docs(sml): add method documentation for sml

--- src/sml.rs
+++ src/sml.rs
@@ -32,22 +32,55 @@ pub const TSDB_SML_TIMESTAMP_NOT_CONFIGURED: TSDB_SML_TIMESTAMP_TYPE =
 pub const TSDB_SML_TIMESTAMP_HOURS: TSDB_SML_TIMESTAMP_TYPE = TSDB_SML_TIMESTAMP_TYPE::Hours;
 pub const TSDB_SML_TIMESTAMP_MINUTES: TSDB_SML_TIMESTAMP_TYPE = TSDB_SML_TIMESTAMP_TYPE::Minutes;
 pub const TSDB_SML_TIMESTAMP_SECONDS: TSDB_SML_TIMESTAMP_TYPE = TSDB_SML_TIMESTAMP_TYPE::Seconds;
-pub const TSDB_SML_TIMESTAMP_MILLI_SECONDS: TSDB_SML_TIMESTAMP_TYPE =
+pub const TSDB_SML_TIMESTAMP_MILLISECONDS: TSDB_SML_TIMESTAMP_TYPE =
     TSDB_SML_TIMESTAMP_TYPE::Milliseconds;
-pub const TSDB_SML_TIMESTAMP_MICRO_SECONDS: TSDB_SML_TIMESTAMP_TYPE =
+pub const TSDB_SML_TIMESTAMP_MICROSECONDS: TSDB_SML_TIMESTAMP_TYPE =
     TSDB_SML_TIMESTAMP_TYPE::Microseconds;
-pub const TSDB_SML_TIMESTAMP_NANO_SECONDS: TSDB_SML_TIMESTAMP_TYPE =
+pub const TSDB_SML_TIMESTAMP_NANOSECONDS: TSDB_SML_TIMESTAMP_TYPE =
     TSDB_SML_TIMESTAMP_TYPE::Nanoseconds;
 
 impl Taos {
-    /// Create stmt with sql
+    /// Schemaless insert with different protocol and timestamp precision.
+    ///
+    /// - InfluxDB line protocol
+    ///
+    ///     ```ignore,rust
+    ///     let lines = ["st,t1=abc,t2=def,t3=anything c1=3i64,c3=L\"pass\",c2=false,c4=4f64 1626006833639000000"];
+    ///     taos.schemaless_insert(&lines, TSDB_SML_LINE_PROTOCOL, TSDB_SML_TIMESTAMP_NANOSECONDS)?;
+    ///     ```
+    ///
+    /// - OpenTSDB telnet protocol
+    ///
+    ///     ```ignore,rust
+    ///     let lines = ["sys.if.bytes.out 1479496100 1.3E3 host=web01 interface=eth0"];
+    ///     taos.schemaless_insert(&lines, TSDB_SML_LINE_PROTOCOL, TSDB_SML_TIMESTAMP_SECONDS)?;
+    ///     ```
+    ///
+    /// - or OpenTSDB json protocol.
+    ///
+    ///     ```ignore,rust
+    ///     let lines = [r#"
+    ///         {
+    ///             "metric":   "st",
+    ///             "timestamp":        1626006833,
+    ///             "value":    10,
+    ///             "tags":     {
+    ///                 "t1":   true,
+    ///                 "t2":   false,
+    ///                 "t3":   10,
+    ///                 "t4":   "123_abc_.!@#$%^&*:;,./?|+-=()[]{}<>"
+    ///             }
+    ///         }"#];
+    ///     taos.schemaless_insert(&lines, TSDB_SML_LINE_PROTOCOL, TSDB_SML_TIMESTAMP_SECONDS)?;
+    ///     ```
+    ///
     pub fn schemaless_insert(
         &self,
         lines: &[impl ToCString],
         protocol: TSDB_SML_PROTOCOL_TYPE,
         precision: TSDB_SML_TIMESTAMP_TYPE,
     ) -> Result<i32, TaosError> {
-        let lines: Vec<_> = lines.into_iter().map(|line| line.to_c_string()).collect();
+        let lines: Vec<_> = lines.iter().map(|line| line.to_c_string()).collect();
         let mut lines = lines
             .iter()
             .map(|line| line.as_ptr() as *mut i8)
@@ -85,9 +118,13 @@ mod test {
         taos.exec(format!("create database if not exists {} keep 36500", db))
             .await?;
         taos.exec(format!("use {}", db)).await?;
+
+        let lines = ["st,t1=abc,t2=def,t3=anything c1=3i64,c3=L\"pass\",c2=false,c4=4f64 1626006833639000000"];
         let res = taos.schemaless_insert(
-            &["st,t1=abc,t2=def,t3=anything c1=3i64,c3=L\"pass\",c2=false,c4=4f64 1626006833639000000"],
-                TSDB_SML_LINE_PROTOCOL, sml::TSDB_SML_TIMESTAMP_TYPE::NonConfigured)?;
+            &lines,
+            TSDB_SML_LINE_PROTOCOL,
+            TSDB_SML_TIMESTAMP_NOT_CONFIGURED,
+        )?;
         assert_eq!(res, 1);
 
         let res = taos.query("select * from st").await?;
@@ -115,11 +152,8 @@ mod test {
             "sys.if.bytes.out 1479496400 3.5E3 host=web04 interface=eth3",
         ];
 
-        let res = taos.schemaless_insert(
-            &lines,
-            TSDB_SML_TELNET_PROTOCOL,
-            sml::TSDB_SML_TIMESTAMP_TYPE::Seconds,
-        )?;
+        let res =
+            taos.schemaless_insert(&lines, TSDB_SML_TELNET_PROTOCOL, TSDB_SML_TIMESTAMP_SECONDS)?;
         assert_eq!(res, 4);
 
         let res = taos.query("select * from `sys.if.bytes.out`").await?;
@@ -152,13 +186,10 @@ mod test {
                 "t4":   "123_abc_.!@#$%^&*:;,./?|+-=()[]{}<>"
             }
         }"#];
-        let res: serde_json::Value = serde_json::from_str(lines[0]).unwrap();
+        let _: serde_json::Value = serde_json::from_str(lines[0]).unwrap();
 
-        let res = taos.schemaless_insert(
-            &lines,
-            TSDB_SML_JSON_PROTOCOL,
-            sml::TSDB_SML_TIMESTAMP_TYPE::Seconds,
-        )?;
+        let res =
+            taos.schemaless_insert(&lines, TSDB_SML_JSON_PROTOCOL, TSDB_SML_TIMESTAMP_SECONDS)?;
         assert_eq!(res, 1);
 
         let res = taos.query("select * from st").await?;
